cf21d8edecad14cdd2052b521d7535abc0f9edaf
Iko Setiawan <mericosetiawan@gmail.com>
2020-10-15 15:18:07 +0700

    Filter Pria dan Perempuan

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const data = "data.json";
 const listMhs = document.querySelector('#mhs-list');
+var urlParams = new URLSearchParams(window.location.search);
+var jk =   urlParams.get("jk");
 
 const getListMhs =()=>{
     fetch(data)
@@ -7,7 +9,11 @@ const getListMhs =()=>{
         return respone.json();
     }).then(responeJson =>{
         console.log(responeJson.mahasiswa);
-        showListMhs(responeJson.mahasiswa);
+        if(jk == null){
+            showListMhs(responeJson.mahasiswa);
+        }else {
+            showListMhsFromGender(responeJson.mahasiswa);
+        }
     }).catch(error =>{
         console.error(error);
     }); 
@@ -37,7 +43,29 @@ const showListMhs = mhs => {
 }
 
 var showListMhsFromGender = mhs => {
-    
+    listMhs.innerHTML="";
+    mhs.forEach(item => {
+        if(item.jk == jk)
+        {
+            listMhs.innerHTML += `
+            <div class="row">
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="${item.foto}">
+                        <span class="card-title">${item.nim}</span>
+                    </div>
+                    <div class="card-content">
+                        <p>Nama : ${item.nama}</p>
+                        <p>Alamat Asal : ${item.alamat_asal}</p>
+                        <p>Alamat Di Malang : ${item.alamat_mlg}</p>
+                        <p>Jenis Kelamin : ${item.jk}</p>
+                    </div>
+                </div>
+            </div>
+            </div>`
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', getListMhs);
